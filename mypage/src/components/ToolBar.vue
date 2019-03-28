e81376fbc06b3d12d1660baba84b191914b9a3f9
김성재 <suzkim@hivelab.co.kr>
2019-03-28 19:10:15 +0900

    [] - 초기 세팅

--- mypage/src/components/ToolBar.vue
+++ mypage/src/components/ToolBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="nav">
-    <h1><router-link to="/main">MyPage</router-link></h1>
+    <h1><router-link to="/">MyPage</router-link></h1>
     <ul>
     <li><router-link to="/menu01">메뉴1</router-link></li>
     <li><router-link to="/menu02">메뉴2</router-link></li>
